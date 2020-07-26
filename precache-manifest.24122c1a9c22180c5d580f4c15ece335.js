708eec6bcb18825524d6f2c4e804a910dddbbd8e
Hung Dau <darklord2065@gmail.com>
2020-07-26 19:46:16 +0700

    Updates

---
+++ precache-manifest.24122c1a9c22180c5d580f4c15ece335.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a48fbd73390c71e0576b02e4afc28cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6eff8a9cf7567e1319dc",
+    "url": "/static/css/main.0730f6e2.chunk.css"
+  },
+  {
+    "revision": "50eaf61e1bb301e36c72",
+    "url": "/static/js/2.8ee60031.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8ee60031.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6eff8a9cf7567e1319dc",
+    "url": "/static/js/main.f5406d01.chunk.js"
+  },
+  {
+    "revision": "405f430893446efaf6a2",
+    "url": "/static/js/runtime-main.9129b20f.js"
+  }
+]);
