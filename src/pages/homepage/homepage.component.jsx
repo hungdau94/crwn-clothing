697a79a045f2eed881e15983a4f267682406d1ee
Hung Dau <darklord2065@gmail.com>
2020-07-31 02:57:13 +0700

    Lesson 7

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './homepage.style.scss';
 import DirectoryMenu from "../../components/directory-menu/directory-menu.component";
+import Header from "../../components/header/header.component"
 
 
 const HomePage = () => (
