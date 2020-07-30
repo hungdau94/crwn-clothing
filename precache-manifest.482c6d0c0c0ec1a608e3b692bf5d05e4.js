ea3e3ee5838f2d3657191feec379f3c6f886884e
Hung Dau <darklord2065@gmail.com>
2020-07-31 02:59:41 +0700

    Updates

---
+++ precache-manifest.482c6d0c0c0ec1a608e3b692bf5d05e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ee1195db3f6d0fa5a6ef21812729353",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "0261c8e63925fffff94d",
+    "url": "/crwn-clothing/static/css/main.34d0395e.chunk.css"
+  },
+  {
+    "revision": "ef8197b403193045f959",
+    "url": "/crwn-clothing/static/js/2.4862d60d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/crwn-clothing/static/js/2.4862d60d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0261c8e63925fffff94d",
+    "url": "/crwn-clothing/static/js/main.1e4b61b0.chunk.js"
+  },
+  {
+    "revision": "0c0bf0a54c9e5596672f",
+    "url": "/crwn-clothing/static/js/runtime-main.5ffcf0f1.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crwn-clothing/static/media/crown.ed28accb.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/crwn-clothing/precache-manifest.0283d85fa6586b18fdbac8f265691afa.js"
+  "/crwn-clothing/precache-manifest.482c6d0c0c0ec1a608e3b692bf5d05e4.js"
 );
 
 self.addEventListener('message', (event) => {
