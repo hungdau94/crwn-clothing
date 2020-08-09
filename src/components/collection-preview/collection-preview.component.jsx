9914d2d4f97c630e040e664c72dd0076d9aaaeb0
Hung Dau <darklord2065@gmail.com>
2020-08-09 17:35:22 +0700

    cart util class for reducer

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -2,19 +2,21 @@ import React from 'react';
 import CollectionItem from "../collection-item/collection-item.component.jsx";
 import "./collection-preview.styles.scss"
 
-const CollectionPreview = ({title, items}) => (
-    <div className='collection-preview'>
-        <div className='title'>
-            {title.toUpperCase()}
+const CollectionPreview = ({title, items}) => {
+    return (
+        <div className='collection-preview'>
+            <div className='title'>
+                {title.toUpperCase()}
+            </div>
+            <div className='preview'>
+                {items
+                    .filter((item, idx) => idx < 4)
+                    .map((item) => (
+                        <CollectionItem key={item.id} item={item}/>
+                    ))}
+            </div>
         </div>
-        <div className='preview'>
-            {items
-                .filter((item, idx) => idx < 4)
-                .map(({id, ...itemProps}) => (
-                    <CollectionItem key={id} {...itemProps}/>
-                ))}
-        </div>
-    </div>
-);
+    )
+};
 
 export default CollectionPreview;
