03217cef4547b9c1b4a9fdd95aa6ad97b46e97d3
Hung Dau <darklord2065@gmail.com>
2020-08-11 17:49:24 +0700

    Fix collection-preview import

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 import "./collections-overview.styles.scss"
-import CollectionPreview from "../../pages/shop/shoppage.component";
 import {createStructuredSelector} from "reselect";
 import {selectCollections} from "../../redux/shop/shop.selectors";
 import {connect} from "react-redux";
+import CollectionPreview from "../collection-preview/collection-preview.component";
 
 const CollectionsOverview = ({collections}) => (
     <div className='collection-overview'>
