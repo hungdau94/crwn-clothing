399efd351a5ab3027f13c285c2d34861b9c42654
Hung Dau <darklord2065@gmail.com>
2020-08-13 10:12:20 +0700

    fix categoryId => collection in Route + data normalization

--- src/pages/category/category.component.jsx
+++ src/pages/category/category.component.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 
 import "./category.styles.scss"
+import {connect} from "react-redux";
+import {createStructuredSelector} from "reselect";
+import {selectCollection, selectCollections} from "../../redux/shop/shop.selectors";
+import CollectionItem from "../../components/collection-item/collection-item.component";
 
-const CategoryPage = ({match}) => (
+const CategoryPage = ({collection: {items, title}}) => (
     <div className='category'>
         <div className='category-page'>
-            <h2>CATEGORY PAGE</h2>
+            <h2>{title}</h2>
+            <div className='category-items'>
+                {
+                    items.map(item => <CollectionItem key={item.id} item={item}/>)
+                }
+            </div>
         </div>
     </div>
 );
 
 
+const mapStateToProps = (state, ownProps) => ({
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
 
-export default CategoryPage;
+export default connect(mapStateToProps)(CategoryPage);
--- src/pages/shop/shoppage.component.jsx
+++ src/pages/shop/shoppage.component.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
-import CategoryPage from "../category/category.component";
+import CollectionPage from "../../pages/collections/collection.component"
 
 const ShopPage = ({match: {path}}) => (
     <div className='shop-page'>
-        <Route path={`${path}`} component={CollectionsOverview} />
-        <Route exact path={`${path}/:categoryId`} component={CategoryPage}></Route>
+        <Route exact path={`${path}`} component={CollectionsOverview} />
+        <Route exact path={`${path}/:collectionId`} component={CollectionPage}></Route>
     </div>
 );
 
